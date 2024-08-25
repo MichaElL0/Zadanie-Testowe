1826487321d05d4b6e6641206c92af0c13857108
mik <54302018+MichaElL0@users.noreply.github.com>
2024-08-25 07:54:21 +0200

    Add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    CharacterController characterController;
 
-    // Update is called once per frame
-    void Update()
+    public float movementSpeed = 10f;
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.5f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+	private void Awake()
+	{
+		characterController = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xAxis + transform.forward * zAxis;
+        characterController.Move(move * movementSpeed * Time.deltaTime);
+
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
